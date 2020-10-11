b59a5369edae26b09401e05831dccbe3b0cc5656
Aleksander Mikucki <aleksander.mikucki@swmansion.com>
2020-10-11 20:15:01 +0200

    Rename controller

--- gupb/default_config.py
+++ gupb/default_config.py
@@ -1,21 +1,39 @@
 from gupb.controller import keyboard
 from gupb.controller import random
-from gupb.controller import myawesome
+from gupb.controller import ihavenoideawhatimdoing
 
 # KEYBOARD_CONTROLLER = keyboard.KeyboardController()
-MY_AWESOME_CONTROLLER = myawesome.MyAwesomeController()
+I_HAVE_NO_IDEA_WHAT_IM_DOING_CONTROLLER = ihavenoideawhatimdoing.IHaveNoIdeaWhatImDoingController()
 CONFIGURATION = {
     'arenas': [
         'island',
     ],
     'controllers': [
         # KEYBOARD_CONTROLLER,
-        MY_AWESOME_CONTROLLER,
+        I_HAVE_NO_IDEA_WHAT_IM_DOING_CONTROLLER,
         random.RandomController("Alice"),
-        # random.RandomController("Bob"),
-        # random.RandomController("Cecilia"),
+        random.RandomController("Bob"),
+        random.RandomController("Cecilia"),
+        random.RandomController("Alice"),
+        random.RandomController("Bob"),
+        random.RandomController("Cecilia"),
+         random.RandomController("Alice"),
+        random.RandomController("Bob"),
+        random.RandomController("Cecilia"),
+         random.RandomController("Alice"),
+        random.RandomController("Bob"),
+        random.RandomController("Cecilia"),
+         random.RandomController("Alice"),
+        random.RandomController("Bob"),
+        random.RandomController("Cecilia"),
+         random.RandomController("Alice"),
+        random.RandomController("Bob"),
+        random.RandomController("Cecilia"),
+        random.RandomController("Alice"),
+        random.RandomController("Bob"),
+        random.RandomController("Cecilia"),
     ],
     'visualise': True,
-    'show_sight': MY_AWESOME_CONTROLLER,
+    'show_sight': I_HAVE_NO_IDEA_WHAT_IM_DOING_CONTROLLER,
     'runs_no': 1,
 }
